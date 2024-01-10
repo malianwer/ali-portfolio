c5ecfc98a019267af35658fe37c210323dbc7603
Ali <mac@MACs-MacBook-Pro.local>
2024-01-11 02:04:42 +0500

    deploy code

--- src/components/Techstack.js
+++ src/components/Techstack.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { TbBrandGithubFilled } from "react-icons/tb";
 import { SiVisualstudio, SiXcode } from "react-icons/si";
 import { DiJavascript1, DiReact } from "react-icons/di";
+import { FaGithub } from "react-icons/fa";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGithubFilled />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
+        <FaGithub />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudio />
